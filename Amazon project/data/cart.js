b2a0aa3e764d023221471b67ba10b9d3597f73b2
Czkafek <czkafek@gmail.com>
2024-08-25 20:09:06 +0200

    Lesson 15 complete

--- Amazon project/data/cart.js
+++ Amazon project/data/cart.js
@@ -1,4 +1,12 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [{productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', quantity: 2}, {productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1}];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+    deliveryOptionId: '1'
+}, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+    deliveryOptionId: '2'
+}];
 
 function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
@@ -20,7 +28,8 @@ export function addToCart(productId) {
     else{
         cart.push({
             productId,
-            quantity
+            quantity,
+            deliveryOptionId: '1'
         });
     }
 
@@ -59,4 +68,18 @@ export function updateQuantity(productId, newQuantity) {
     });
 
     saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    cart.forEach((item) => {
+        if (productId === item.productId) {
+            matchingItem = item;
+        }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    saveToStorage(); 
 }
