300405fb682e181137b76c6db17e4bdfd57fae34
Czkafek <czkafek@gmail.com>
2024-08-26 15:52:45 +0200

    jasmine is fucked, I'm gonna play mc

--- Amazon project/data/cart.js
+++ Amazon project/data/cart.js
@@ -1,12 +1,18 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-}];
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart')) || [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2,
+        deliveryOptionId: '1'
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2'
+    }];
+}
 
 function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
@@ -14,7 +20,8 @@ function saveToStorage() {
 
 export function addToCart(productId) {
     let matchingItem;
-    let quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+    let quantity = 1;
+    //let quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
 
     cart.forEach((item) => {
         if (productId === item.productId) {
@@ -83,3 +90,4 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
 
     saveToStorage(); 
 }
+
